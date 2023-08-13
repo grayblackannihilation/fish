5161ceb9373fb269e5080b42c8c97d67e050fe29
era3effulgence <123359284+grayblackannihilation@users.noreply.github.com>
2023-08-13 19:43:02 +0800

    1

--- js/plugins/skiptitle.js
+++ js/plugins/skiptitle.js
@@ -1,19 +1,10 @@
 // 在遊戲加載完成後立即運行的代碼
 Scene_Boot.prototype.start = function() {
     Scene_Base.prototype.start.call(this);
-    SoundManager.preloadImportantSounds();
-    if (DataManager.isBattleTest()) {
-        DataManager.setupBattleTest();
-        SceneManager.goto(Scene_Battle);
-    } else if (DataManager.isEventTest()) {
-        DataManager.setupEventTest();
-        SceneManager.goto(Scene_Map);
-    } else {
+   
         this.checkPlayerLocation();
         // 設置存檔2的遊戲狀態
         DataManager.loadGame(2);
         // 跳轉到地圖畫面
         SceneManager.goto(Scene_Map);
     }
-    this.updateDocumentTitle();
-};
